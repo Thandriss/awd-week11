50ae1633032b4594befe52013b95cc61668bb068
Thandriss <mahonko_yana@mail.ru>
2024-01-11 14:53:02 +0200

    5

--- src/components/MyHOC.js
+++ src/components/MyHOC.js
@@ -3,11 +3,11 @@ const MyHOC = (Component, name) => {
     const MeComp = (props) => {
         return(
             <div className="wrapper">
-                <Component name={name.name}></Component>
+                <Component name={name.name} {...props}></Component>
             </div>
         )
     }
-    return MeComp
+    return <MeComp/>
 
 }
 export default MyHOC;
