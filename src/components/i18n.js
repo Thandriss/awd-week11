577d65f6c02bd7b96f88352a964991ccbcf764d5
Thandriss <mahonko_yana@mail.ru>
2024-01-11 12:47:07 +0200

    3

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -1,10 +1,10 @@
 import i18n from "i18next";
-import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import translationFI from './locales/fi/translation.json';
 import translationEN from './locales/en/translation.json';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-// the translations
 const resources = {
   fi: {
     translation: translationFI
@@ -15,16 +15,16 @@ const resources = {
 };
 
 i18n
-  .use(detector)
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: "en", // use en if detected lng is not available
-
-    keySeparator: false, // we do not use keys in form messages.welcome
+    fallbackLng: 'en',
+    debug: true,
 
     interpolation: {
-      escapeValue: false // react already safes from xss
+      escapeValue: false, 
     }
   });
 
